c723efa73d1d0330757c9e719653c003d74238a4
Marco Vinicius <marcoviniciusmoreiramacedo@gmail.com>
2021-06-28 20:17:00 -0300

    Primeiro commit. Já configurado com restify, mongoose e bcrypt. Projeto já está na metade, com validações, recursos e configurações prontas.

---
+++ server/merge-patch.parsers.ts
@@ -0,0 +1,18 @@
+import * as restify from "restify"
+import { BadRequestError } from "restify-errors";
+
+const mpContentType = "application/merge-patch+json"
+
+export const mergePatchBodyParser = (req: restify.Request, resp: restify.Response, next) => {
+    if (req.getContentType() === mpContentType && req.method === "PATCH") {
+        (<any>req).rawBody = req.body;
+
+        try {
+            req.body = JSON.parse(req.body)
+        } catch (error) {
+            return next(new BadRequestError(`Invalid content: ${error.message}`))
+        }
+    }
+
+    return next()
+}
